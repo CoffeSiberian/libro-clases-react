ebf16376765a32d55361db3ce6d0ac10a9928b0d
SiberianCoffe <siberiancoffe@outlook.cl>
2022-12-03 15:40:38 -0300

    infinite loop solved

--- src/components/ValidUser.js
+++ src/components/ValidUser.js
@@ -1,29 +1,34 @@
-import { useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 
 const ValidUser = ({ localToken, children }) => {
-	const [userStatus, setUserStatus] = useState(null);
 	const { bodySet } = useFetch(
 		`${process.env.REACT_APP_APIURL}/validate`,
 		"POST",
 		{
-			"Authorization": `Bearer ${localToken}`,
+			Authorization: `Bearer ${localToken}`,
 		}
 	);
+	const loaded = useRef(false);
+	const aut = useRef(null);
 
+	const validateToken = async () => {
+		let fetchResponse = await bodySet();
+		if (fetchResponse.status === 200) return (aut.current = true);
+		return (aut.current = false);
+	};
+	
 	useEffect(() => {
-		const validateToken = async (token) => {
-			let fetchResponse = await bodySet(token);
-			if (fetchResponse.status === 200) return setUserStatus(true);
-			return setUserStatus(false);
-		};
-		validateToken(localToken);
-	}, [localToken, bodySet]);
+		if (!loaded.current) {
+			validateToken();
+			loaded.current = true;
+		}
+	}, []);
 
-	if (userStatus === false) {
+	if (aut.current === false) {
 		return <Navigate to={"/login"} replace={true} />;
-	} else if (userStatus === null) return <h1>loading...</h1>;
+	} else if (aut.current === null) return <h1>loading...</h1>;
 	return children;
 };
 
