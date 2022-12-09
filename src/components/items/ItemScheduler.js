7ed75322734d51d154bc84c40775978b7d22cba8
SiberianCoffe <siberiancoffe@outlook.cl>
2022-12-09 15:27:40 -0300

    date format to schedule

--- src/components/items/ItemScheduler.js
+++ src/components/items/ItemScheduler.js
@@ -2,16 +2,39 @@ import Divider from "@mui/material/Divider";
 import { Typography } from "@mui/material";
 
 const ItemSchedule = ({ name, grade, dateStart, dateEnd, objetive }) => {
+	const options = {
+		dateStyle: "full",
+	};
+	const optionsHour = {
+		hour12: false,
+		hour: "numeric",
+		minute: "numeric",
+	};
+
+	const dateStartObj = new Date(dateStart).toLocaleDateString("es-CL", options);
+	const HourStartObj = new Date(dateStart).toLocaleTimeString(
+		"es-CL",
+		optionsHour
+	);
+	const HourEndObj = new Date(dateEnd).toLocaleTimeString("es-CL", optionsHour);
+
 	return (
 		<div className="shadow-xl m-5 p-2 border-2 border-inherit rounded-md">
 			<div className="grid grid-cols-2 items-center">
 				<Typography className="text-right" variant="subtitle1">
-					Name:
+					Materia:
 				</Typography>
 				<div className="ml-2">
 					<Typography variant="body2">{name}</Typography>
 				</div>
 			</div>
+
+			<Divider
+				className="flexn"
+				orientation="horizontal"
+				variant="middle"
+				flexItem
+			/>
 
 			<div className="grid grid-cols-2 items-center">
 				<Typography className="text-right" variant="subtitle1">
@@ -28,28 +51,24 @@ const ItemSchedule = ({ name, grade, dateStart, dateEnd, objetive }) => {
 				variant="middle"
 				flexItem
 			/>
-			<div className="grid grid-cols-2 items-center">
-				<Typography className="text-right" variant="subtitle1">
-					Inicio Clase:
-				</Typography>
-				<div className="ml-2">
-					<Typography variant="body2">{dateStart}</Typography>
-				</div>
-			</div>
 
-			<Divider
-				className="flex"
-				orientation="horizontal"
-				variant="middle"
-				flexItem
-			/>
-			<div className="grid grid-cols-2 items-center">
+			<div className="grid justify-center">
 				<Typography className="text-right" variant="subtitle1">
-					Fin Clase:
+					Horario
 				</Typography>
-				<div className="ml-2">
-					<Typography variant="body2">{dateEnd}</Typography>
-				</div>
+			</div>
+			<div className="text-center space-y-2 mb-2">
+				<Typography variant="body2">{dateStartObj}</Typography>
+				<Typography
+					className="flex justify-center space-x-1"
+					variant="body2"
+					component="div"
+				>
+					<p>Desde las</p>
+					<p className="text-red-700">{HourStartObj}</p>
+					<p>hasta las</p>
+					<p className="text-green-700">{HourEndObj}</p>
+				</Typography>
 			</div>
 
 			<Divider
